2db1f8afa94081f6a0edbaa0f73b89c8b4f0c643
WildCLown <32888444+WildCLown@users.noreply.github.com>
2022-05-05 14:53:26 +0000

    [LeetCode Sync] Runtime - 93 ms (6.22%), Memory - 42.1 MB (100.00%)

---
+++ LeetCode/0225-implement-stack-using-queues/solution.js
@@ -0,0 +1,60 @@
+
+var MyStack = function() {
+    this.q1 = [];
+    this.q2 = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+    this.q1.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+    let aux;
+    while(this.q1.length > 1){
+        this.q2.push(this.q1.shift());
+    }
+    aux = this.q1.shift();
+    while(this.q2.length > 0){
+        this.q1.push(this.q2.shift());
+    }
+    return aux;
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+    let aux;
+    while(this.q1.length > 1){
+        this.q2.push(this.q1.shift());
+    }
+    aux = this.q1[0];
+    this.q2.push(this.q1.shift());
+    while(this.q2.length > 0){
+        this.q1.push(this.q2.shift());
+    }
+    return aux;
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+    return this.q1.length == 0;
+};
+
+/** 
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
